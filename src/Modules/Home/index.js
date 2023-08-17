27ffd0654750b9b65cb79e6310f1220990239ed0
Le Hieu <firephoenix.shinji@gmail.com>
2023-08-17 15:49:37 +0700

    Loading Page

--- src/Modules/Home/index.js
+++ src/Modules/Home/index.js
@@ -1,36 +1,52 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import "./styles.scss";
+import LoadingPage from "@/components/Loading/LoadingPage";
 
 function HomePage() {
+  const [toggle, setToggle] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setToggle(false);
+    }, 5000);
+  });
+
   return (
     <>
-      <div className="imgLoader"></div>
+      {toggle ? (
+        <LoadingPage />
+      ) : (
+        <div>
+          <div className="imgLoader"></div>
 
-      <div className="container">
-        <h1 className="title">
-          Turning pages
-          <br />
-          with css
-        </h1>
+          <div className="container">
+            <h1 className="title">
+              Turning pages
+              <br />
+              with css
+            </h1>
 
-        <div className="book">
-          <div className="gap"></div>
-          <div className="pages">
-            <div className="page"></div>
-            <div className="page"></div>
-            <div className="page"></div>
-            <div className="page"></div>
-            <div className="page"></div>
-            <div className="page"></div>
-          </div>
-          <div className="flips">
-            <div className="flip flip1">
-              <div className="flip flip2">
-                <div className="flip flip3">
-                  <div className="flip flip4">
-                    <div className="flip flip5">
-                      <div className="flip flip6">
-                        <div className="flip flip7"></div>
+            <div className="book">
+              <div className="gap"></div>
+              <div className="pages">
+                <div className="page"></div>
+                <div className="page"></div>
+                <div className="page"></div>
+                <div className="page"></div>
+                <div className="page"></div>
+                <div className="page"></div>
+              </div>
+              <div className="flips">
+                <div className="flip flip1">
+                  <div className="flip flip2">
+                    <div className="flip flip3">
+                      <div className="flip flip4">
+                        <div className="flip flip5">
+                          <div className="flip flip6">
+                            <div className="flip flip7"></div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -38,20 +54,21 @@ function HomePage() {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div>
-        <div className="table center">
-          <div className="monitor-wrapper center">
-            <div className="monitor center">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
+          <div>
+            <div className="table center">
+              <div className="monitor-wrapper center">
+                <div className="monitor center">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
---
+++ src/components/Loading/LoadingPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.scss";
+
+function LoadingPage() {
+  return (
+    <div className="wrapper">
+      <div className="loading">
+        <span>Loading</span>
+      </div>
+    </div>
+  );
+}
+
+export default LoadingPage;
